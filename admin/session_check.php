5e1588cf766fa22eda6c600b9d71f714168d1e99
Aidil Akbar <160083195+Aidilakbar10@users.noreply.github.com>
2025-12-07 11:42:54 +0700

    Delete admin directory

--- admin/session_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// Jika tidak ada session admin atau levelnya bukan SUPERADMIN/EDITOR, tendang ke halaman login
-if (!isset($_SESSION['admin_id'])) {
-    header('Location: login.php?error=Anda harus login terlebih dahulu.');
-    exit();
-}
-?>
